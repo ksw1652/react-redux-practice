bb4fe058694473eabde465fe9b01cf48cf8d68b3
ksw1652 <ksw1652@gmail.com>
2021-03-17 19:47:37 +0900

    feat: change movie and get user list by redux, redux-thunk

---
+++ src/store/index.js
@@ -0,0 +1,26 @@
+import {createStore, combineReducers, compose, applyMiddleware} from "redux";
+import movielistReducer from "./reducers/movielistReducer";
+import userReducer from "./reducers/userReducer";
+import thunk from 'redux-thunk';
+
+const middleware = [thunk];
+
+const allReducers = combineReducers({
+  movies: movielistReducer, users: userReducer
+});
+
+const initialState = {
+  movies: {name: "TERMINATOR 2"},
+  users: []
+};
+
+const store = createStore(
+  allReducers,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default store;
